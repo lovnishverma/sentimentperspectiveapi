da755da6f09159676f816c75e85ab64e7a7e7307
Glitch (flasktestapp) <none>
2023-08-31 17:24:50 +0000

    🏚🐔 Checkpoint ./app.py:57629255/2092 ./templates/index.html:57629255/2276 ./requirements.txt:57629255/11

--- app.py
+++ app.py
@@ -1,46 +1,26 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
+from textblob import TextBlob
 
 app = Flask(__name__)
 
-# Handle the default web page request
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        url = request.form['url']
-        tag = request.form['tag']
-        if url and tag:
-            json_data = scrape(url, tag)
-            return render_template("index.html", headings=json_data['headings'], url=url, tag=tag)
-    return render_template("index.html")
+    return render_template('index.html')
 
-# Function to scrape a page
-def scrape(url, tag):
-    try:
-        # Make the HTTP request to the URL to get the data
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        data = response.content
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    text = request.form['text']
+    blob = TextBlob(text)
+    sentiment_score = blob.sentiment.polarity
 
-        # Load the HTML into BeautifulSoup web scraper
-        soup = BeautifulSoup(data, 'html.parser')
+    if sentiment_score > 0:
+        sentiment = 'Positive'
+    elif sentiment_score < 0:
+        sentiment = 'Negative'
+    else:
+        sentiment = 'Neutral'
 
-        # Create a dictionary to store the scraped data
-        json_data = {'headings': []}
+    return render_template('index.html', text=text, sentiment=sentiment)
 
-        # Search for the elements of the specified tag
-        selection = soup.find_all(tag)
-
-        # Add the elements to the dictionary
-        for el in selection:
-            text = el.get_text()
-            json_data['headings'].append(text)
-
-        return json_data
-    except Exception as e:
-        return {'error': str(e)}
-
-# Start the app running on the web server
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
