fa3630963549fed5b41ae5302dfba8240a0ff90b
Glitch (flasktestapp) <none>
2023-08-31 17:47:56 +0000

    🐉🖇 Checkpoint ./templates/index.html:57629255/2024 ./app.py:57629255/8000 ./requirements.txt:57629255/63 ./.glitch-assets:57629255/512 ./react-ef60e-firebase-adminsdk-hqoze-e399bdac16.json:57629255/2373

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
-from flask import Flask, render_template, request
-from textblob import TextBlob
+from flask import Flask, render_template, request, jsonify
+import os
+from googleapiclient import discovery
+import google.auth
 
 app = Flask(__name__)
 
-@app.route('/')
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "react-ef60e-firebase-adminsdk-hqoze-e399bdac16.json"  # Set your service account key path
+credentials, _ = google.auth.default()
+api_service = discovery.build('commentanalyzer', 'v1alpha1', credentials=credentials)
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    text = request.form['text']
-    blob = TextBlob(text)
-    sentiment_score = blob.sentiment.polarity
+@app.route("/sort-lyrics", methods=["POST"])
+def sort_lyrics():
+    lyrics = request.form["lyrics"]
+    lyrics_split = lyrics.split("\n")
+    
+    lyrics_scored = []
+    for line in lyrics_split:
+        if line:
+            analyze_request = {
+                "comment": {"text": line},
+                "requestedAttributes": {"TOXICITY": {}},
+            }
+            response = api_service.comments().analyze(body=analyze_request).execute()
+            toxicity_score = response["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
+            lyrics_scored.append({"text": line, "toxicity": toxicity_score})
+    
+    return jsonify(lyrics_scored)
 
-    if sentiment_score > 0:
-        sentiment = 'Positive'
-    elif sentiment_score < 0:
-        sentiment = 'Negative'
-    else:
-        sentiment = 'Neutral'
-
-    return render_template('index.html', text=text, sentiment=sentiment)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
